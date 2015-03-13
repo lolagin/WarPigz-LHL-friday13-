fa884d5e104e04c67d63d9e2aeb541396abf78df
Jeffrey Rosenthal <jeffrey.rosenthal@gmail.com>
2015-03-13 11:28:20 -0700

    Initial Commit

---
+++ DirtyPig/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DirtyPig
+//
+//  Created by Jeffrey C Rosenthal on 3/13/15.
+//  Copyright (c) 2015 Jeffreycorp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
